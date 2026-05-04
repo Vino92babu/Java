688f3a0e6dc911b30d7d9b4aa064b1b6327ad628
vino92babu <vino92babu@gmail.com>
2026-05-05 00:05:25 +0530

    Initial commit

---
+++ src/CoreJava/Firstclass.java
@@ -0,0 +1,13 @@
+package CoreJava;
+
+public class Firstclass {
+	static int a =66;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vinoth");
+		int A = 5;
+		System.out.println(a);
+	}
+
+}
